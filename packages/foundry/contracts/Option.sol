c57388634ca6d34416f225880d4e5822370c2bef
Mahmoud Lababidi <mahmoud@mahmoud.one>
2025-05-31 02:04:18 -0400

    lint contracts

--- packages/foundry/contracts/Option.sol
+++ packages/foundry/contracts/Option.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import {IPermit2} from "./interaces/IPermit2.sol";
+import { IPermit2 } from "./interaces/IPermit2.sol";
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
@@ -14,7 +14,7 @@ using SafeERC20 for IERC20;
 // The Long Option contract is the owner of the Short Option contract
 // The Long Option contract is the only one that can mint new options
 // The Long Option contract is the only one that can exercise options
-// The redemption is only possible if you own both the Long and Short Option contracts but 
+// The redemption is only possible if you own both the Long and Short Option contracts but
 // performed by the Long Option contract
 
 // In options traditionally a Consideration is cash and a Collateral is an asset
@@ -24,19 +24,19 @@ using SafeERC20 for IERC20;
 // or even staked stable coins can be used as well for either consideration or collateral.
 
 contract OptionBase is ERC20, Ownable, ReentrancyGuard {
-
     IPermit2 public constant PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
     string public _name;
     string public _symbol;
-    uint256 public  expirationDate;
-    uint256 public  strike;
-    uint256 public constant STRIKE_DECIMALS = 10**18;
+    uint256 public expirationDate;
+    uint256 public strike;
+    uint256 public constant STRIKE_DECIMALS = 10 ** 18;
     // The strike price includes the ratio of the consideration to the collateral
     // and the decimal difference between the consideration and collateral along
-    // with the strike decimals of 18. 
+    // with the strike decimals of 18.
     bool public isPut;
     IERC20 public collateral;
     IERC20 public consideration;
+
     error ContractNotExpired();
     error ContractExpired();
     error InsufficientBalance();
@@ -49,7 +49,7 @@ contract OptionBase is ERC20, Ownable, ReentrancyGuard {
 
     modifier notExpired() {
         if (block.timestamp >= expirationDate) revert ContractExpired();
-        
+
         _;
     }
 
@@ -63,21 +63,15 @@ contract OptionBase is ERC20, Ownable, ReentrancyGuard {
         _;
     }
 
-
     constructor(
-        string memory name_, 
-        string memory symbol_, 
-        address collateral_, 
+        string memory name_,
+        string memory symbol_,
+        address collateral_,
         address consideration_,
-        uint256 expirationDate_, 
+        uint256 expirationDate_,
         uint256 strike_,
         bool isPut_
-        ) 
-        ERC20(name_, symbol_) 
-        Ownable(msg.sender) 
-        ReentrancyGuard() {
-
-
+    ) ERC20(name_, symbol_) Ownable(msg.sender) ReentrancyGuard() {
         if (collateral_ == address(0)) revert InvalidValue();
         if (consideration_ == address(0)) revert InvalidValue();
         if (strike_ == 0) revert InvalidValue();
@@ -88,45 +82,39 @@ contract OptionBase is ERC20, Ownable, ReentrancyGuard {
         isPut = isPut_;
         collateral = IERC20(collateral_);
         consideration = IERC20(consideration_);
-        }
+    }
 
-        function toConsideration(uint256 amount) public view returns (uint256 ) {
-            // The strike price actually contains the ratio of Consideration
-            // over Collateral including the decimals associated. The ratio is 
-            // multiplied by 10**18 as is the standard convention. That's why 
-            // we eventually divide by the STRIKE_DECIMALS. MulDiv?
-            return (amount * strike)/ STRIKE_DECIMALS;
-        }
+    function toConsideration(uint256 amount) public view returns (uint256) {
+        // The strike price actually contains the ratio of Consideration
+        // over Collateral including the decimals associated. The ratio is
+        // multiplied by 10**18 as is the standard convention. That's why
+        // we eventually divide by the STRIKE_DECIMALS. MulDiv?
+        return (amount * strike) / STRIKE_DECIMALS;
+    }
 
-        function init(
-            string memory name_, 
-            string memory symbol_,
-            address collateral_,
-            address consideration_,
-            uint256 expirationDate_, 
-            uint256 strike_,
-            bool isPut_
-        ) public onlyOwner() {
-            _name = name_;
-            _symbol = symbol_;
-            collateral = IERC20(collateral_);
-            consideration = IERC20(consideration_);
-            expirationDate = expirationDate_;
-            strike = strike_;
-            isPut = isPut_;
-        }
-
+    function init(
+        string memory name_,
+        string memory symbol_,
+        address collateral_,
+        address consideration_,
+        uint256 expirationDate_,
+        uint256 strike_,
+        bool isPut_
+    ) public onlyOwner {
+        _name = name_;
+        _symbol = symbol_;
+        collateral = IERC20(collateral_);
+        consideration = IERC20(consideration_);
+        expirationDate = expirationDate_;
+        strike = strike_;
+        isPut = isPut_;
+    }
 }
 
 contract ShortOption is OptionBase {
-
     address public longOption;
 
-    event Redemption(
-        address longOption,
-        address holder,
-        uint256 amount
-    );
+    event Redemption(address longOption, address holder, uint256 amount);
 
     modifier sufficientCollateral(address contractHolder, uint256 amount) {
         if (collateral.balanceOf(contractHolder) < amount) {
@@ -136,72 +124,101 @@ contract ShortOption is OptionBase {
     }
 
     constructor(
-        string memory name, 
-        string memory symbol, 
-        address collateral, 
+        string memory name,
+        string memory symbol,
+        address collateral,
         address consideration,
-        uint256 expirationDate, 
+        uint256 expirationDate,
         uint256 strike,
         bool isPut
-        ) OptionBase(name, symbol, collateral, consideration, expirationDate, strike, isPut) {
-        }
-    function setLongOption(address longOption_) public onlyOwner() {
+    ) OptionBase(name, symbol, collateral, consideration, expirationDate, strike, isPut) { }
+
+    function setLongOption(address longOption_) public onlyOwner {
         longOption = longOption_;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner sufficientCollateral(to, amount) validAmount(amount) notExpired {
+    function mint(address to, uint256 amount)
+        public
+        onlyOwner
+        sufficientCollateral(to, amount)
+        validAmount(amount)
+        notExpired
+    {
         __mint(to, amount);
     }
 
-    function __mint(address to, uint256 amount) private nonReentrant sufficientCollateral(to, amount) validAmount(amount) {
+    function __mint(address to, uint256 amount)
+        private
+        nonReentrant
+        sufficientCollateral(to, amount)
+        validAmount(amount)
+    {
         collateral.safeTransferFrom(to, address(this), amount);
         _mint(to, amount);
     }
 
-    function mint2(address to, uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature) 
-    public onlyOwner sufficientCollateral(to, amount) validAmount(amount) notExpired {
+    function mint2(address to, uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature)
+        public
+        onlyOwner
+        sufficientCollateral(to, amount)
+        validAmount(amount)
+        notExpired
+    {
         PERMIT2.permit(to, permitDetails, signature);
         _mint2(to, amount);
     }
 
-    function _mint2(address to, uint256 amount) private nonReentrant sufficientCollateral(to, amount) validAmount(amount) {
+    function _mint2(address to, uint256 amount)
+        private
+        nonReentrant
+        sufficientCollateral(to, amount)
+        validAmount(amount)
+    {
         PERMIT2.transferFrom(to, address(this), uint160(amount), address(collateral));
         _mint(to, amount);
     }
 
-    function _redeem(address to, uint256 amount) private nonReentrant sufficientBalance(to, amount) validAmount(amount){
-
+    function _redeem(address to, uint256 amount)
+        private
+        nonReentrant
+        sufficientBalance(to, amount)
+        validAmount(amount)
+    {
         uint256 collateralBalance = collateral.balanceOf(address(this));
         uint256 considerationBalance = consideration.balanceOf(address(this));
-        
+
         // First try to fulfill with collateral
         uint256 collateralToSend = amount <= collateralBalance ? amount : collateralBalance;
-        
+
         // Burn the redeemed tokens
         _burn(to, amount);
         // If we couldn't fully fulfill with collateral, try to fulfill remainder with consideration
         if (collateralToSend < amount) {
             uint256 remainingAmount = amount - collateralToSend;
             uint256 considerationNeeded = toConsideration(remainingAmount);
-            
+
             // Verify we have enough consideration tokens; this should never happen
             if (considerationBalance < considerationNeeded) {
                 revert InsufficientBalance();
             }
-            
+
             // Transfer consideration tokens for the remaining amount
             consideration.safeTransfer(to, considerationNeeded);
         }
-        
+
         // Transfer whatever collateral we can
         if (collateralToSend > 0) {
             collateral.safeTransfer(to, collateralToSend);
         }
         emit Redemption(address(longOption), to, amount);
-
     }
 
-    function _redeemConsideration(address to, uint256 amount) private nonReentrant sufficientBalance(to, amount) validAmount(amount){
+    function _redeemConsideration(address to, uint256 amount)
+        private
+        nonReentrant
+        sufficientBalance(to, amount)
+        validAmount(amount)
+    {
         // Verify we have enough consideration tokens
         uint256 considerationAmount = toConsideration(amount);
         if (consideration.balanceOf(address(this)) < considerationAmount) revert InsufficientBalance();
@@ -209,7 +226,6 @@ contract ShortOption is OptionBase {
         // Transfer consideration tokens for the remaining amount
         consideration.safeTransfer(to, considerationAmount);
         emit Redemption(address(longOption), to, amount);
-
     }
 
     function redeem(uint256 amount) public expired sufficientBalance(msg.sender, amount) {
@@ -220,30 +236,35 @@ contract ShortOption is OptionBase {
         _redeemConsideration(msg.sender, amount);
     }
 
-    function _redeemPair(address to, uint256 amount) public notExpired onlyOwner() sufficientBalance(to, amount) {
+    function _redeemPair(address to, uint256 amount) public notExpired onlyOwner sufficientBalance(to, amount) {
         _redeem(to, amount);
     }
 
-    function _exercise(address contractHolder, uint256 amount) private nonReentrant notExpired onlyOwner() {
+    function _exercise(address contractHolder, uint256 amount) private nonReentrant notExpired onlyOwner {
         uint256 considerationAmount = toConsideration(amount);
         if (consideration.balanceOf(contractHolder) < considerationAmount) revert InsufficientBalance();
         consideration.safeTransferFrom(contractHolder, address(this), considerationAmount);
         collateral.safeTransfer(contractHolder, amount);
     }
 
-    function exercise(address contractHolder, uint256 amount) public notExpired onlyOwner() {
+    function exercise(address contractHolder, uint256 amount) public notExpired onlyOwner {
         _exercise(contractHolder, amount);
     }
 
-    function _exercise2(address contractHolder, uint256 amount) public notExpired onlyOwner() nonReentrant {
+    function _exercise2(address contractHolder, uint256 amount) public notExpired onlyOwner nonReentrant {
         uint256 considerationAmount = toConsideration(amount);
         if (consideration.balanceOf(contractHolder) < considerationAmount) revert InsufficientBalance();
-        
+
         PERMIT2.transferFrom(contractHolder, address(this), uint160(considerationAmount), address(consideration));
         collateral.safeTransfer(contractHolder, amount);
     }
 
-    function exercise2(address contractHolder, uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature) public notExpired onlyOwner() {
+    function exercise2(
+        address contractHolder,
+        uint256 amount,
+        IPermit2.PermitSingle calldata permitDetails,
+        bytes calldata signature
+    ) public notExpired onlyOwner {
         PERMIT2.permit(contractHolder, permitDetails, signature);
         _exercise2(contractHolder, amount);
     }
@@ -251,20 +272,14 @@ contract ShortOption is OptionBase {
     function sweep(address holder) public expired sufficientBalance(holder, balanceOf(holder)) {
         _redeem(holder, balanceOf(holder));
     }
-
 }
 
 contract LongOption is OptionBase {
     ShortOption public shortOption;
 
+    event Exercise(address longOption, address holder, uint256 amount);
 
-    event Exercise(
-        address longOption,
-        address holder,
-        uint256 amount
-    );
-
-    constructor (
+    constructor(
         string memory name,
         string memory symbol,
         address collateral,
@@ -273,26 +288,21 @@ contract LongOption is OptionBase {
         uint256 strike,
         bool isPut,
         address shortOptionAddress_
-    ) OptionBase(
-        name, 
-        symbol, 
-        collateral, 
-        consideration, 
-        expirationDate, 
-        strike, 
-        isPut
-    ) {
+    ) OptionBase(name, symbol, collateral, consideration, expirationDate, strike, isPut) {
         shortOption = ShortOption(shortOptionAddress_);
-
-
-    } 
+    }
 
     function mint(uint256 amount) public nonReentrant validAmount(amount) notExpired {
         _mint(msg.sender, amount);
         shortOption.mint(msg.sender, amount);
     }
 
-    function mint2(uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature) public nonReentrant validAmount(amount) notExpired {
+    function mint2(uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature)
+        public
+        nonReentrant
+        validAmount(amount)
+        notExpired
+    {
         _mint(msg.sender, amount);
         shortOption.mint2(msg.sender, amount, permitDetails, signature);
     }
@@ -303,18 +313,24 @@ contract LongOption is OptionBase {
         emit Exercise(address(this), msg.sender, amount);
     }
 
-    function exercise2(uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature) public notExpired nonReentrant validAmount(amount) {
+    function exercise2(uint256 amount, IPermit2.PermitSingle calldata permitDetails, bytes calldata signature)
+        public
+        notExpired
+        nonReentrant
+        validAmount(amount)
+    {
         _burn(msg.sender, amount);
         shortOption.exercise2(msg.sender, amount, permitDetails, signature);
         emit Exercise(address(this), msg.sender, amount);
     }
 
-    function redeem(uint256 amount) 
-        public 
-        notExpired 
+    function redeem(uint256 amount)
+        public
+        notExpired
         nonReentrant
-        sufficientBalance(msg.sender, amount) 
-        validAmount(amount){
+        sufficientBalance(msg.sender, amount)
+        validAmount(amount)
+    {
         if (shortOption.balanceOf(msg.sender) < amount) revert InsufficientBalance();
 
         address contractHolder = msg.sender;
@@ -324,22 +340,21 @@ contract LongOption is OptionBase {
 }
 
 contract OptionFactory is Ownable {
-
     address public shortContract;
     address public longContract;
 
     event OptionCreated(
         address longOption,
         address shortOption,
-        address collateral, 
+        address collateral,
         address consideration,
-        uint256 expirationDate, 
+        uint256 expirationDate,
         uint256 strike,
         bool isPut
     );
 
     address[] public createdOptions;
-    mapping (uint256 => address[])  public shortLong;
+    mapping(uint256 => address[]) public shortLong;
 
     mapping(address => mapping(uint256 => mapping(uint256 => address[]))) public allOptions;
 
@@ -349,58 +364,33 @@ contract OptionFactory is Ownable {
     }
 
     function createOption(
-        string memory longOptionName, 
+        string memory longOptionName,
         string memory shortOptionName,
         string memory longSymbol,
         string memory shortSymbol,
-        address collateral, 
-        address consideration, 
-        uint256 expirationDate, 
+        address collateral,
+        address consideration,
+        uint256 expirationDate,
         uint256 strike,
         bool isPut
-        ) public {
-
+    ) public {
         address short = Clones.clone(shortContract);
         address long = Clones.clone(longContract);
 
         ShortOption shortOption = ShortOption(short);
         LongOption longOption = LongOption(long);
 
-        shortOption.init(
-            shortOptionName, 
-            shortSymbol, 
-            collateral, 
-            consideration, 
-            expirationDate, 
-            strike,
-            isPut
-        );
+        shortOption.init(shortOptionName, shortSymbol, collateral, consideration, expirationDate, strike, isPut);
 
-        longOption.init(
-            longOptionName, 
-            longSymbol, 
-            collateral, 
-            consideration, 
-            expirationDate, 
-            strike, 
-            isPut
-        );
-        
+        longOption.init(longOptionName, longSymbol, collateral, consideration, expirationDate, strike, isPut);
+
         createdOptions.push(long);
         allOptions[collateral][expirationDate][strike].push(long);
         shortOption.setLongOption(long);
         shortOption.transferOwnership(long);
         longOption.transferOwnership(owner());
 
-        emit OptionCreated(
-            long,
-            short, 
-            collateral, 
-            consideration,
-            expirationDate, 
-            strike,
-            isPut
-        );
+        emit OptionCreated(long, short, collateral, consideration, expirationDate, strike, isPut);
     }
 
     function getCreatedOptions() public view returns (address[] memory) {
@@ -409,5 +399,4 @@ contract OptionFactory is Ownable {
     // function getOption(address collateral, uint256 expiration, uint256 strike) public view returns (address[] memory) {
     //     return allOptions[collateral][expiration][strike];
     // }
-
 }
--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -25,26 +25,11 @@ contract DeployYourContract is ScaffoldETHDeploy {
      *      - Export contract addresses & ABIs to `nextjs` packages
      */
     function run() external ScaffoldEthDeployerRunner {
-
-        ShortOption short = new ShortOption(
-            "Short Option",
-            "SHORT",
-            deployer,
-            deployer,
-            block.timestamp + 1 days,
-            100,
-            false
-        );
+        ShortOption short =
+            new ShortOption("Short Option", "SHORT", deployer, deployer, block.timestamp + 1 days, 100, false);
 
         LongOption long = new LongOption(
-            "Long Option",
-            "LONG",
-            deployer,
-            deployer,
-            block.timestamp + 1 days,
-            100,
-            false,
-            address(short)
+            "Long Option", "LONG", deployer, deployer, block.timestamp + 1 days, 100, false, address(short)
         );
 
         new OptionFactory(address(short), address(long));
